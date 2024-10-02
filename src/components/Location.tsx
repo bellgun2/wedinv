eef46bcbebe315b07f8da7237eb7e97c6bf91049
bellgun2 <bellgun@amc.seoul.kr>
2024-10-02 16:24:13 +0900

    Location.tsx 업데이트

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -27,6 +27,12 @@ export default function Location() {
       <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
         <Title>오시는 길</Title>
       </Divider>
+      <Text>
+        서울 중구 을지로 30 롯데호텔서울 본관 38층<br />
+        2호선 을지로입구역 출구 113m<br />
+        로비 도착시 좌측의 무궁화 전용 엘레베이터를<br />
+        이용하시면 보다 편안히 오실 수 있습니다.
+      </Text>
       <Image src="./assets/LocationMap.png" />
     </Wrapper>
   );
