76a5c87749b98979120608ebc6248c95822cd30f
bellgun2 <bellgun@amc.seoul.kr>
2024-10-02 14:27:08 +0900

    Add files via upload

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,43 @@
+import ImageGallery from "react-image-gallery";
+import { Divider } from "antd";
+import { styled } from "@stitches/react";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const Wrapper = styled("div", {
+  background: "#efebe9",
+  backgroundImage: "url(./assets/GroovePaper.png)",
+  width: "100%",
+});
+
+const Title = styled("p", {
+  fontSize: "2vh",
+  fontWeight: "bold",
+  opacity: 0.85,
+  marginBottom: 0,
+});
+
+const images = [
+  {
+    original: "./assets/Gallery_Photo_1.png",
+    thumbnail: "./assets/Gallery_Photo_1.png",
+  },
+  {
+    original: "./assets/Gallery_Photo_2.png",
+    thumbnail: "./assets/Gallery_Photo_2.png",
+  },
+];
+
+export default function Gallery() {
+  return (
+    <Wrapper>
+      <Divider plain style={{ marginTop: 0, marginBottom: 32 }}>
+        <Title>Gallery</Title>
+      </Divider>
+      <ImageGallery
+        showPlayButton={false}
+        showFullscreenButton={false}
+        items={images}
+      />
+    </Wrapper>
+  );
+}
